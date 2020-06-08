73fad76db9e29dff9cfb7795f1563e1e544d22b6
MatheusAlves1010 <66440541+MatheusAlves1010@users.noreply.github.com>
2020-06-07 22:10:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function pesquisar(){
     
     window.location.href = pais + interesse + ".html";
 }
+
+function vazio1() {
+    var pais;
+    pais = document.getElementById("paises").value;
+    if ((pais == "Escolha um país")||(pais == null)) {
+       return false;
+    };
+ }
+
+ function vazio2() {
+    var interesse;
+    interesse = document.getElementById("interesses").value;
+    if ((interesse == "Escolha um interesse")||(interesse == null)) {
+       return false;
+    };
+ }
+
+ function verificar(){
+     if(vazio1() == false){
+        alert("Selecione um país");
+     }else if(vazio2() == false){
+        alert("Selecione um interesse");
+     }else{
+        pesquisar();
+     }
+ }
